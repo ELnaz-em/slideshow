13781d89092ecbbfb3435c43de759e60b032127e
ELnaz-em <elnaz.moradi@maersk.com>
2022-05-19 13:26:32 +0200

    getting images using axios

--- slideshow/src/App.tsx
+++ slideshow/src/App.tsx
@@ -7,24 +7,59 @@ const App = () => {
   const [altImages, setAltImages] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  async function getImages() {
+  const axios = require("axios").default;
+  // Make a request for a user with a given ID
+  function getImages() {
     const url = "https://picsum.photos/v2/list";
     setIsLoading(true);
-    try {
-      const response = await fetch(url);
-      const jsonData = await response.json();
-      const imagesUrl: string[] = [];
-      const imageAlt: string[] = [];
-      jsonData.map((jsonElement: { download_url: string; author: string }) => {
-        imagesUrl.push(jsonElement.download_url);
-        imageAlt.push(jsonElement.author);
+    axios
+      .get(url)
+      .then(function (response:any) {
+        // handle success
+        const jsonData = response.data;
+        const imagesUrl: string[] = [];
+        const imageAlt: string[] = [];
+        console.log("*********: "+jsonData);
+
+        jsonData.map((jsonElement: { download_url: string; author: string }) => {
+                imagesUrl.push(jsonElement.download_url);
+                imageAlt.push(jsonElement.author);
+               });
+              setImages(imagesUrl);
+              setAltImages(imageAlt)
+      })
+      .catch(function (error: any) {
+        // handle error
+        console.log("getting images err: " + error);
+      })
+      .then(function () {
+        // always executed
+        setIsLoading(false);
       });
-      setImages(imagesUrl);
-    } catch (err) {
-      console.log("Fetching images err: " + err);
-    }
-    setIsLoading(false);
   }
+
+  /*
+  get images using fetches
+  */
+  // async function getImages() {
+  //   const url = "https://picsum.photos/v2/list";
+  //   setIsLoading(true);
+  //   try {
+  //     const jsonData = await fetch(url);
+  //     const jsonData = await jsonData.json();
+  //     const imagesUrl: string[] = [];
+  //     const imageAlt: string[] = [];
+  //     jsonData.map((jsonElement: { download_url: string; author: string }) => {
+  //       imagesUrl.push(jsonElement.download_url);
+  //       imageAlt.push(jsonElement.author);
+  //     });
+  //     setImages(imagesUrl);
+  //     setAltImages(imageAlt)
+  //   } catch (err) {
+  //     console.log("Fetching images err: " + err);
+  //   }
+  //   setIsLoading(false);
+  // }
   useEffect(() => {
     getImages();
   }, []);
@@ -51,7 +86,7 @@ const App = () => {
             src={images[index]}
             width="400"
             height="341"
-            alt="CPH Harbor"
+            alt={altImages[index]}
           />
           <br />
 
